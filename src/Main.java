8b5928ce325b1698e90fa727c5780845ac59970f
kylian <kylianclerc@fedora>
2023-10-16 12:00:18 +0200

    Tictactoe

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // initialise char pour afficher le tableau
+        char [][] tableau = {
+                {'1','2','3'},
+                {'4','5','6'},
+                {'7','8','9'}
+        };
+
+        // initialisation d'un int pour le jouer
+        int tour = 1;
+
+
+
+
     }
+
+
+
+
+
+
+
+
+
 }
