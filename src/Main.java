5d99df6442e664b64fb3dd5dabc1cac8c8f91bbd
kylian <kylianclerc@fedora>
2023-10-30 10:25:44 +0100

    update function displays

--- src/Main.java
+++ src/Main.java
@@ -50,7 +50,13 @@ public class Main {
                 }
 
             }
+            System.out.println();
+
+            if (line < 2) {
+                System.out.println("---------");
+            }
         }
+        System.out.println();
     }
 
 
